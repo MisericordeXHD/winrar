4ea666a75cf3fde65e053b73f6ff6db5c23df1ca
MisericordeXHD <145233261+MisericordeXHD@users.noreply.github.com>
2023-09-17 10:22:58 +0200

    Create readme.ml

---
+++ readme.ml
@@ -0,0 +1,81 @@
+# 无广告 WinRAR 官方简体中文版
+## 简介
+> 从5.4正式版之后，WinRAR简体中文版代理商在部分简体中文版本程序中内置了即使购买了授权也无法去除的广告页面，本页面列举了从5.5之后的可去除广告的WinRAR简体中文正式版下载链接。  
+## 关于WinRAR官网的识别  
+开发商RARLAB：https://www.rarlab.com/  
+发行商win.rar GmbH：https://www.win-rar.com/  
+简体中文代理商：http://www.winrar.com.cn/  
+
+> rrlb:指从 RaRLaB.com 购买 ，wrr:指从 Win-RaR.com 购买，仅说明文件存在区别，程序无区别。
+## 链接  
+### 5.4
+**32位：**  http://www.win-rar.com/fileadmin/winrar-versions/sc20160819/wrr/wrar540sc.exe  
+**64位：**  http://www.win-rar.com/fileadmin/winrar-versions/sc20160819/wrr/winrar-x64-540sc.exe  
+
+### 5.5  
+**32位：** http://www.win-rar.com/fileadmin/winrar-versions/sc20170830/wrr/wrar550sc.exe  
+**64位：** http://www.win-rar.com/fileadmin/winrar-versions/sc20170830/wrr/winrar-x64-550sc.exe  
+
+### 5.6  
+**32位：** http://www.win-rar.com/fileadmin/winrar-versions/sc20180711/wrr/wrar560sc.exe  
+**64位：** http://www.win-rar.com/fileadmin/winrar-versions/sc20180711/wrr/winrar-x64-560sc.exe  
+
+### 5.61  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc20181016/wrr/wrar561sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc20181016/wrr/winrar-x64-561sc.exe  
+
+### 5.7  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc20190304/wrr/wrar570sc.exe  
+**64位：** http://www.win-rar.com/fileadmin/winrar-versions/sc20190304/wrr/winrar-x64-570sc.exe  
+
+### 5.71  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc20190509/wrr/wrar571sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc20190509/wrr/winrar-x64-571sc.exe  
+
+### 5.8  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20191217/rrlb/wrar580sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20191217/rrlb/winrar-x64-580sc.exe  
+ 
+### 5.9  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20200409/rrlb/wrar590sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20200409/rrlb/winrar-x64-590sc.exe  
+
+### 5.91
+~~**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20200706/rrlb/wrar591sc.exe~~  
+~~**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20200706/rrlb/winrar-x64-591sc.exe~~  
+
+旧版证书被吊销，更新的地址   
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20200827/wrr/wrar591sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20200827/wrr/winrar-x64-591sc.exe  
+
+### 6.00  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20201210/wrr/wrar600sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20201210/wrr/winrar-x64-600sc.exe  
+
+### 6.01  
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20210414/rrlb/wrar601sc.exe   
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20210414/rrlb/winrar-x64-601sc.exe  
+
+### 6.02
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20210616/rrlb/wrar602sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20210616/rrlb/winrar-x64-602sc.exe  
+
+### 6.10
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20220127/wrr/winrar-x32-610sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20220127/wrr/winrar-x64-610sc.exe  
+
+### 6.11
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20220317/wrr/winrar-x32-611sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20220317/wrr/winrar-x64-611sc.exe  
+
+### 6.21
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20230223/wrr/winrar-x32-621sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20230223/wrr/winrar-x64-621sc.exe  
+
+### 6.22 
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20230607/wrr/winrar-x32-622sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20230607/wrr/winrar-x64-622sc.exe  
+
+### 6.23
+**32位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20230808/wrr/winrar-x32-623sc.exe  
+**64位：** https://www.win-rar.com/fileadmin/winrar-versions/sc/sc20230808/wrr/winrar-x64-623sc.exe  
